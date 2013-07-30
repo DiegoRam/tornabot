b492808871ae3f41cf8ebe0ac68f86959a3334f9
diegoram <ddarkheart@gmail.com>
2013-07-29 21:05:18 -0300

    add static folder and index jade template

--- app-tornabot.py
+++ app-tornabot.py
@@ -1,13 +1,29 @@
 #imports
-import tornado
+import os.path
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado.options import define,options
 from tornado import template
 from pyjade.ext.tornado import patch_tornado
 
+
 patch_tornado()
+define("port", default=8000, help="run on the given port",type=int)
 
 class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
-		render('index.jade')
+		self.render('index.jade')
 
 if __name__ == '__main__':
-	main()
+	tornado.options.parse_command_line()
+	app = tornado.web.Application(
+		handlers=[(r"/",IndexHandler),(r"/favicon.ico", tornado.web.StaticFileHandler,{'path':'static'})],
+		template_path=os.path.join(os.path.dirname(__file__),"templates"),
+		static_path=os.path.join(os.path.dirname(__file__),"static"),
+		debug=True	
+	)
+	http_server = tornado.httpserver.HTTPServer(app)
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()
